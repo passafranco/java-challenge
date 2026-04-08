511c052133c39c5e889d273a75c0d0695873ad00
Franco Passarelli <fpassarelli@itpatagonia.com>
2026-04-08 20:56:26 -0300

    incorporar Lombok y simplificar modelo Transaction
    Se agrega dependencia Lombok y se reemplaza el boilerplate
    de getters/setters/constructores por anotaciones.

--- src/main/java/com/mendel/challenge/model/Transaction.java
+++ src/main/java/com/mendel/challenge/model/Transaction.java
@@ -1,51 +1,18 @@
 package com.mendel.challenge.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Transaction {
 
     private Long id;
     private Double amount;
     private String type;
     private Long parentId;
-
-    public Transaction() {
-    }
-
-    public Transaction(Long id, Double amount, String type, Long parentId) {
-        this.id = id;
-        this.amount = amount;
-        this.type = type;
-        this.parentId = parentId;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Double getAmount() {
-        return amount;
-    }
-
-    public void setAmount(Double amount) {
-        this.amount = amount;
-    }
-
-    public String getType() {
-        return type;
-    }
-
-    public void setType(String type) {
-        this.type = type;
-    }
-
-    public Long getParentId() {
-        return parentId;
-    }
-
-    public void setParentId(Long parentId) {
-        this.parentId = parentId;
-    }
 }
